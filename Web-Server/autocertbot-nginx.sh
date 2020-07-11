8d24cc81c2d17878c8648c8f1cd45fe0ce0c27ce
omarabdalhamid <33742475+omarabdalhamid@users.noreply.github.com>
2020-07-11 17:55:53 +0200

    Update and rename auto-certbot-nginx.sh to autocertbot-nginx.sh

---
+++ Web-Server/autocertbot-nginx.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+################################################################################
+# Script for installing ZiSoft on Ubuntu 14.04, 15.04, 16.04 and 18.04 (could be used for other version too)
+# Author: OmarAbdalhamid Omar
+################################################################################
+
+echo "\n#############################################"
+
+echo  "\n--- Generate SSL CERT --"
+
+echo "\n#############################################"
+
+
+
+read -p  "Enter Domain Name : "  domain_name
+
+echo "\n"
+wget https://dl.eff.org/certbot-auto
+sudo mv certbot-auto /usr/local/bin/certbot-auto
+sudo chown root /usr/local/bin/certbot-auto
+sudo chmod 0755 /usr/local/bin/certbot-auto
+service nginx stop
+sudo certbot-auto certonly   --standalone -d $domain_name
+service nginx start
