dbc72b8a2b28f2087fda080eda9b3449c8279784
omarabdalhamid <33742475+omarabdalhamid@users.noreply.github.com>
2020-07-11 17:51:26 +0200

    Create auto-certbot-nginx.sh

---
+++ Web-Server/auto-certbot-nginx.sh
@@ -0,0 +1,8 @@
+read -p  "Enter Domain Name : "  domain_name
+wget https://dl.eff.org/certbot-auto
+sudo mv certbot-auto /usr/local/bin/certbot-auto
+sudo chown root /usr/local/bin/certbot-auto
+sudo chmod 0755 /usr/local/bin/certbot-auto
+service nginx stop
+certbot-auto certonly   --standalone -d $domain_name
+service nginx start
